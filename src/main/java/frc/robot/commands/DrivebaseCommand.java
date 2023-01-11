5f22536a952aae2d0b4ff0b21bbcb3ae0f9a0139
Jay <jaykang445@gmail.com>
2023-01-10 17:41:08 -0800

    add drive in command

--- src/main/java/frc/robot/commands/DrivebaseCommand.java
+++ src/main/java/frc/robot/commands/DrivebaseCommand.java
@@ -11,31 +11,29 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 public class DrivebaseCommand extends CommandBase {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
   private final DrivebaseSubsystem m_subsystem;
+  private double speedY, speedX, rotX;
 
-  /**
-   * Creates a new ExampleCommand.
-   *
-   * @param subsystem The subsystem used by this command.
-   */
-  public DrivebaseCommand(DrivebaseSubsystem subsystem) {
+  public DrivebaseCommand(DrivebaseSubsystem subsystem, double speedY, double speedX, double rotX) {
+    
+    this.speedY = speedY;
+    this.speedX = speedX;
+    this.rotX = rotX;
+
     m_subsystem = subsystem;
-    // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
   }
 
-  // Called when the command is initially scheduled.
   @Override
   public void initialize() {}
 
-  // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    m_subsystem.driveMecanum(speedY, speedX, rotX);
+  }
 
-  // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {}
 
-  // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     return false;
