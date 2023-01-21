6787339e4b77a7189a2cbc7a56206d5e8e183154
Jay <jaykang445@gmail.com>
2023-01-21 15:40:04 -0800

    set up angle command

--- src/main/java/frc/robot/commands/AngleDriveCommand.java
+++ src/main/java/frc/robot/commands/AngleDriveCommand.java
@@ -1,9 +1,51 @@
 package frc.robot.commands;
 
+import java.util.function.DoubleSupplier;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DrivebaseSubsystem;
 
 public class AngleDriveCommand extends CommandBase{
+
+    DrivebaseSubsystem subsystem;
+    DoubleSupplier leftX, leftY, rightX, rightY;
+    double angle;
     
-    
+    public AngleDriveCommand(DrivebaseSubsystem subsystem, DoubleSupplier leftX, DoubleSupplier leftY, DoubleSupplier rightX, DoubleSupplier rightY) {
+        
+        this.subsystem = subsystem;
+        this.leftX = leftX;
+        this.leftY = leftY;
+        this.rightX = rightX;
+        this.rightY = rightY;
+
+        addRequirements(subsystem);
+    }
+
+    @Override
+    public void initialize() {
+
+        angle = subsystem.getGyroRotation().getDegrees();
+
+    }
+
+    @Override
+    public void execute() {
+
+        if( > 0.7) {
+
+        }
+
+    }
+
+    @Override 
+    public void end(boolean interrupted) {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
 
 }
