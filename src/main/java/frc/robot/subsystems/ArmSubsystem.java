40514f4307c312fd1974a144e0804d13c7618687
Derek Chen <derekchen1815@gmail.com>
2023-01-24 20:04:42 -0800

    Added Shuffleboard

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,8 +1,9 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
@@ -11,30 +12,28 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class ArmSubsystem extends SubsystemBase{
     private PIDController m_PIDController;
     private TalonFX ArmMotor;
-    private double ArmPosition;
     private double DesiredAngle;
     private ShuffleboardTab ArmShuffleboard = Shuffleboard.getTab("Arm");
 
     public ArmSubsystem(){
-        m_PIDController = new PIDController(0.05, 0, 0);
+        m_PIDController = new PIDController(0.0000001, 0, 0.95);
+        ArmShuffleboard.add("PID", m_PIDController);
+        ArmShuffleboard.addNumber("Arm Angle", ArmMotor::getSelectedSensorPosition);
     }
 
     public double getCurrentPos(){
-        ArmPosition = ArmMotor.getSelectedSensorPosition();
-        return ArmPosition;
+        return ArmMotor.getSelectedSensorPosition();
     }
 
     public void setDesiredAngle(double desiredAngle){
         DesiredAngle = desiredAngle;
     }
 
-    public void initalize(){
-        ArmShuffleboard.add("PID", m_PIDController);
+    public void initialize(){
     }
 
     public void periodic(){
-        ArmPosition = ArmMotor.getSelectedSensorPosition();
-        ArmMotor.set(ControlMode.PercentOutput, m_PIDController.calculate(ArmMotor.getSelectedSensorPosition(), DesiredAngle));
+        ArmMotor.set(TalonFXControlMode.PercentOutput, MathUtil.clamp(m_PIDController.calculate(ArmMotor.getSelectedSensorPosition(), DesiredAngle), -0.1, 0.1));
     }
 
     public void simulationPeriodic(){}
