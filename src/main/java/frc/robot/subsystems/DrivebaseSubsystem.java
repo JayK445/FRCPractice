d77b271511c58fe4c3ca2c258b20b9033d5fe73f
Derek Chen <derekchen1815@gmail.com>
2023-02-11 13:16:20 -0800

    Using WPI_TalonSRX and Mecanum Drive

--- src/main/java/frc/robot/subsystems/DrivebaseSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivebaseSubsystem.java
@@ -4,28 +4,28 @@
 
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.sensors.WPI_PigeonIMU;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.TalonSRXMecanum;
 
 public class DrivebaseSubsystem extends SubsystemBase {
-  private TalonSRXMecanum mecanumDrive;
-  private TalonSRX m_frontLeft, m_frontRight, m_backLeft, m_backRight;
-  private WPI_PigeonIMU gyro;
+  private MecanumDrive drivebase;  private WPI_PigeonIMU gyro;
+  private WPI_TalonSRX frontLeft, frontRight, backLeft, backRight;
 
   public DrivebaseSubsystem() {
-    gyro = new WPI_PigeonIMU(m_frontLeft);
+    gyro = new WPI_PigeonIMU(frontLeft);
     // construct motors
-    m_frontLeft = new TalonSRX(3);
-    m_frontRight = new TalonSRX(1);
-    m_backLeft = new TalonSRX(6);
-    m_backRight = new TalonSRX(7);
-    mecanumDrive = new TalonSRXMecanum(m_frontLeft, m_backLeft, m_frontRight, m_backRight);
+    frontLeft = new WPI_TalonSRX(3);
+    frontRight = new WPI_TalonSRX(1);
+    backLeft = new WPI_TalonSRX(6);
+    backRight = new WPI_TalonSRX(7);
+
+    drivebase = new MecanumDrive(frontLeft, backRight, frontRight, backLeft);
   }
 
   public void drive (double ySpeed, double xSpeed, double zRotation){
-    mecanumDrive.driveCartesian(xSpeed, ySpeed, zRotation, gyro.getRotation2d());
+    drivebase.driveCartesian(xSpeed, ySpeed, zRotation, gyro.getRotation2d());
     /*m_frontLeft.set(ControlMode.PercentOutput, ySpeed);
     m_frontRight.set(ControlMode.PercentOutput, ySpeed);
     m_backLeft.set(ControlMode.PercentOutput, ySpeed);
@@ -33,18 +33,18 @@ public class DrivebaseSubsystem extends SubsystemBase {
   }
 
   public void InvertMotors(){
-    m_frontRight.setInverted(true);
-    m_backRight.setInverted(true);
+    frontRight.setInverted(true);
+    backRight.setInverted(true);
   }
 
   public void UninvertMotors(){
-    m_frontRight.setInverted(false);
-    m_backRight.setInverted(false);
+    frontRight.setInverted(false);
+    backRight.setInverted(false);
   }
 
   public void ToggleInvert(){
-    m_frontRight.setInverted(m_frontRight.getInverted());
-    m_backRight.setInverted(m_backRight.getInverted());
+    frontRight.setInverted(frontRight.getInverted());
+    backRight.setInverted(backRight.getInverted());
   }
   public WPI_PigeonIMU getGyro(){
     return gyro;
