654eead183e432b8d16fe72a853ece0f08b2eb32
Derek Chen <derekchen1815@gmail.com>
2023-02-16 17:06:51 -0800

    Auto Selection

--- src/main/java/frc/robot/commands/SequentialCommand.java
+++ src/main/java/frc/robot/commands/SequentialCommand.java
@@ -3,6 +3,13 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.commands;
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.ArmSubsystem;
@@ -13,7 +20,9 @@ public class SequentialCommand extends SequentialCommandGroup {
   @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
   private DrivebaseSubsystem m_DrivebaseSubsystem;
   private ArmSubsystem m_ArmSubsystem;
-
+  private PathPlannerTrajectory trajectory;
+  private SendableChooser<Command> autoSelector = new SendableChooser<>();
+  private ShuffleboardTab shuffleboard = Shuffleboard.getTab("Auto");
 
   /**
    * Creates a new ExampleCommand.
@@ -22,9 +31,12 @@ public class SequentialCommand extends SequentialCommandGroup {
    */
   
   public SequentialCommand(DrivebaseSubsystem drivebaseSubsystem, ArmSubsystem armSubsystem) {
+    trajectory = PathPlanner.loadPath("New Path", 1, 1);
+    autoSelector.setDefaultOption("Auto Test", new FollowTrajectoryCommand(trajectory, m_DrivebaseSubsystem));
+    shuffleboard.add("Auto Selector", autoSelector);
     m_DrivebaseSubsystem = drivebaseSubsystem;
     m_ArmSubsystem = armSubsystem;
-    addCommands(new TimedDrivebaseCommand(m_DrivebaseSubsystem, 0.3, 0, 0, 1.5), 
+    addCommands(autoSelector.getSelected(), new TimedDrivebaseCommand(m_DrivebaseSubsystem, 0.3, 0, 0, 1.5), 
     new TimedDrivebaseCommand(m_DrivebaseSubsystem, 0.25, 0.3, 0.15, 3), Commands.parallel(
     new TimedDrivebaseCommand(m_DrivebaseSubsystem, -0.1, -0.1, 0, 1), new ArmCommand(m_ArmSubsystem, 90, 0.5))
     );
