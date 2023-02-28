cc91ed6177b507728fa8a58dab12b9fdbdaeaa41
Derek Chen <derekchen1815@gmail.com>
2023-02-28 15:45:19 -0800

    RobotContainer Update

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,14 +3,17 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
-
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import frc.robot.commands.ArmCommand;
 import frc.robot.commands.DrivebaseCommand;
 import frc.robot.commands.SequentialCommand;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.DrivebaseSubsystem;
+import frc.robot.subsystems.ArmSubsystem.Modes;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
@@ -27,12 +30,15 @@ public class RobotContainer {
   private final CommandXboxController controller_1 = new CommandXboxController(0);
   private final DrivebaseSubsystem m_drivebaseSubsystem = new DrivebaseSubsystem();
   private final ArmSubsystem m_armSubsystem = new ArmSubsystem();
+  private SendableChooser<Command> autoSelector = new SendableChooser<>();
+  private ShuffleboardTab commandSelector = Shuffleboard.getTab("Command Selector");
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
-    m_drivebaseSubsystem.setDefaultCommand(new DrivebaseCommand(m_drivebaseSubsystem, controller_1::getLeftY, controller_1::getLeftX, controller_1::getRightY));
+    m_drivebaseSubsystem.setDefaultCommand(new DrivebaseCommand(
+      m_drivebaseSubsystem, controller_1::getLeftY, controller_1::getLeftX, controller_1::getRightX));
   }
 
   /**
@@ -44,8 +50,10 @@ public class RobotContainer {
    */
 
   private void configureButtonBindings() {
-    controller_1.a().onTrue(new ArmCommand(m_armSubsystem, 90, 0.5));
-    controller_1.rightBumper().onTrue((new SequentialCommand(m_drivebaseSubsystem, m_armSubsystem)));
+    controller_1.y().onTrue(autoSelector.getSelected());
+    controller_1.a().onTrue(new ArmCommand(m_armSubsystem, 90, 0.5, Modes.ON));
+    controller_1.b().onTrue(new ArmCommand(m_armSubsystem, 0, 0, Modes.HOLD_POSITION));
+    controller_1.rightBumper().onTrue((new SequentialCommand(m_drivebaseSubsystem, m_armSubsystem, autoSelector.getSelected())));
   }
 
   /**
@@ -56,6 +64,11 @@ public class RobotContainer {
   
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return null;
+    return autoSelector.getSelected();
+  }
+
+  public void setupAutonomousCommand(){
+    autoSelector.setDefaultOption("Auto Test", new SequentialCommand(m_drivebaseSubsystem, m_armSubsystem, autoSelector.getSelected()));
+    commandSelector.add("Auto Selector", autoSelector);
   }
 }
--- src/main/java/frc/robot/commands/SequentialCommand.java
+++ src/main/java/frc/robot/commands/SequentialCommand.java
@@ -3,10 +3,12 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.commands;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.ArmSubsystem;
-import frc.robot.subsystems.DrivebaseSubsystem; 
+import frc.robot.subsystems.DrivebaseSubsystem;
+import frc.robot.subsystems.ArmSubsystem.Modes; 
 
 /** An example command that uses an example subsystem. */
 public class SequentialCommand extends SequentialCommandGroup {
@@ -21,12 +23,12 @@ public class SequentialCommand extends SequentialCommandGroup {
    * @param subsystem The subsystem used by this command.
    */
   
-  public SequentialCommand(DrivebaseSubsystem drivebaseSubsystem, ArmSubsystem armSubsystem) {
+  public SequentialCommand(DrivebaseSubsystem drivebaseSubsystem, ArmSubsystem armSubsystem, Command command) {
     m_DrivebaseSubsystem = drivebaseSubsystem;
     m_ArmSubsystem = armSubsystem;
     addCommands(new TimedDrivebaseCommand(m_DrivebaseSubsystem, 0.3, 0, 0, 1.5), 
     new TimedDrivebaseCommand(m_DrivebaseSubsystem, 0.25, 0.3, 0.15, 3), Commands.parallel(
-    new TimedDrivebaseCommand(m_DrivebaseSubsystem, -0.1, -0.1, 0, 0.5), new ArmCommand(m_ArmSubsystem, 90, 0.5))
+    new TimedDrivebaseCommand(m_DrivebaseSubsystem, -0.1, -0.1, 0, 0.5), new ArmCommand(m_ArmSubsystem, 90, 0.5, Modes.ON))
     );
   }
 }
